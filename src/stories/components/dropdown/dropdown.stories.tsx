9f6c53f8bfa072ef46f7c068a48f12a6cd3932d9
Archie George <archiegeorge23@gmail.com>
2025-05-13 18:57:28 +0100

    dropdown: complete build

--- src/stories/components/dropdown/dropdown.stories.tsx
+++ src/stories/components/dropdown/dropdown.stories.tsx
@@ -8,17 +8,10 @@ const meta = {
     layout: "centered",
   },
   args: {
-    headerLabel: "dropdown",
-    numberOfItems: 5,
     backgroundColor: "black",
     borderRadius: "md",
   },
   argTypes: {
-    headerLabel: {
-      description: "Controls the content of the dropdown",
-      control: { type: "text" },
-      defaultValue: "dropdown",
-    },
     backgroundColor: {
       description: "Controls the background color of the modal",
       control: { type: "color" },
@@ -30,12 +23,6 @@ const meta = {
       options: ["xs", "sm", "md", "lg", "xl"],
       defaultValue: "md",
     },
-    numberOfItems: {
-      name: "Number of dropdowns",
-      control: { type: "select" },
-      options: [1, 2, 3, 4, 5],
-      defaultValue: 5,
-    },
   },
 } satisfies Meta<typeof DropDown>;
 
@@ -43,15 +30,4 @@ export default meta;
 export type Story = StoryObj<typeof DropDown>;
 
 export const Primary: Story = {
-  render: (args) => {
-
-    const itemsToShow = [];
-    for (let i = 0; i < (args.numberOfItems ?? 5); i++) {
-      itemsToShow.push({
-        headerLabel: `dropdown ${i+1}`,
-      });
-    }
-
-    return <DropDown {...args} items={itemsToShow} />;
-  },
 };
--- src/stories/components/dropdown/dropdown.tsx
+++ src/stories/components/dropdown/dropdown.tsx
@@ -2,28 +2,68 @@ import { useState, useRef, useEffect } from "react";
 import { borderRadiusStyles } from "../../utilities/styles";
 
 interface DropDownProps {
-  headerLabel: string;
+  label: string;
   backgroundColor: string;
   borderRadius: string;
-  items: { headerLabel: string }[];
-  numberOfItems?: number;
-  children: string;
 }
 
 const DropDown = ({
-  children = "select",
+  label = "select",
   backgroundColor = "black",
   borderRadius = "md",
-  items = [],
 }: DropDownProps) => {
   const [openDropdown, setOpenDropdown] = useState(false);
-  const [activeFocusedIndex, setActiveFocusedIndex] = useState<number | null>(null);
+  const [activeFocusedIndex, setActiveFocusedIndex] = useState<number | null>(
+    null
+  );
+  const [value, setValue] = useState<number | null>(null);
   const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   const optionRefs = useRef<(HTMLDivElement | null)[]>([]);
+
+  const items = [
+    {
+      name: "Ferrari",
+      title: "Ferrari SF90 Stradale",
+      value: 986,
+    },
+    {
+      name: "Lamborghini",
+      title: "Lamborghini Aventador SVJ",
+      value: 759,
+    },
+    {
+      name: "Mclaren",
+      title: "McLaren 765LT",
+      value: 755,
+    },
+    {
+      name: "Bugatti",
+      title: "Bugatti Chiron Super Sport",
+      value: 1578,
+    },
+    {
+      name: "Porsche",
+      title: "Porsche 911 Turbo S",
+      value: 641,
+    },
+  ];
 
   const handleClick = () => setOpenDropdown((prev) => !prev);
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+
+      if (!openDropdown) {
+        setOpenDropdown(true);
+        setActiveFocusedIndex(0);
+      } else if (activeFocusedIndex !== null) {
+        setSelectedIndex(activeFocusedIndex);
+        setOpenDropdown(false);
+      }
+      return;
+    }
+
     if (!openDropdown) return;
 
     if (e.key === "ArrowDown") {
@@ -36,9 +76,6 @@ const DropDown = ({
       setActiveFocusedIndex((prev) =>
         prev === null || prev === 0 ? items.length - 1 : prev - 1
       );
-    } else if (e.key === "Enter" && activeFocusedIndex !== null) {
-      setSelectedIndex(activeFocusedIndex);
-      setOpenDropdown(false);
     } else if (e.key === "Escape") {
       setOpenDropdown(false);
     }
@@ -50,14 +87,24 @@ const DropDown = ({
     }
   }, [activeFocusedIndex]);
 
-  const displayLabel = selectedIndex !== null ? items[selectedIndex].headerLabel : children;
+  const displayLabel =
+    selectedIndex !== null
+      ? `${items[selectedIndex].title}: ${items[selectedIndex].value} BHP`
+      : label;
+
+  const selectedValue =
+    value !== null
+      ? `${items[value].value}`
+      : "";
+
+  console.log(selectedValue)
 
   return (
     <div
       className="relative inline-block text-left"
       tabIndex={0}
       onKeyDown={handleKeyDown}
-      >
+    >
       <div
         onClick={handleClick}
         className="cursor-pointer inline-flex items-center gap-2 rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold text-white"
@@ -70,13 +117,17 @@ const DropDown = ({
           strokeWidth={2}
           viewBox="0 0 24 24"
         >
-          <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M19 9l-7 7-7-7"
+          />
         </svg>
       </div>
 
       <div
         style={{ ...(backgroundColor && { backgroundColor }) }}
-        className={`absolute w-45 text-right mt-2 transition-all duration-300 ease-in-out z-50 overflow-hidden ${
+        className={`absolute w-65 text-right mt-2 transition-all duration-300 ease-in-out z-50 overflow-hidden ${
           openDropdown ? "opacity-100 max-h-[100rem]" : "opacity-0 max-h-0"
         } ${borderRadiusStyles(borderRadius)}`}
       >
@@ -87,7 +138,9 @@ const DropDown = ({
           return (
             <div
               key={index}
-              ref={(e) => {optionRefs.current[index] = e}}
+              ref={(e) => {
+                optionRefs.current[index] = e;
+              }}
               tabIndex={-1}
               role="option"
               aria-selected={isSelected}
@@ -103,7 +156,9 @@ const DropDown = ({
                 hover:bg-white/20
               `}
             >
-              {item.headerLabel}
+              <div className="w-full bg-transparent border-none outline-none">
+                {item.name}
+              </div>
             </div>
           );
         })}
